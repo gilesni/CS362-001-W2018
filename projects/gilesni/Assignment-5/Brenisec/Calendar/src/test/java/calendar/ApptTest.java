6808960e47366eb28d00ab6ea433248788d765dd
Jklihu <gilesni@oregonstate.edu>
2018-03-12 21:37:54 -0700

    Added and updated the rest of the code.

--- projects/gilesni/Assignment-5/Brenisec/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/gilesni/Assignment-5/Brenisec/Calendar/src/test/java/calendar/ApptTest.java
@@ -1,223 +1,223 @@
 package calendar;
 /**
- *  This class provides a basic set of test cases for the
- *  Appt class.
+ * This class provides a basic set of test cases for the
+ * Appt class.
  */
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
 public class ApptTest {
     /**
      * Test that the gets methods work as expected.
      */
-	@Test
-	public void test01()  throws Throwable  {
-		Appt appointment = new Appt(4, 2, 2, 2, 2000, "T", "D");
-		 
-		//Tests for getStartMonth and setStartMonth
-		appointment.setStartMonth(7);
-		int test = appointment.getStartMonth();
-		assertEquals(7, test);
-		 
-		//Tests for getStartHour and setStartHour
-		appointment.setStartHour(12);
-		test = appointment.getStartHour();
-		assertEquals(12, test);
-		 
-		//Tests for getStartMinute and setStartMinute 
-		appointment.setStartMinute(32);
-		test = appointment.getStartMinute();
-		assertEquals(32, test);
-		 
-		//Tests for getStartDay and setStartDay
-		appointment.setStartDay(2);
-		test = appointment.getStartDay();
-		assertEquals(2, test);
-		 
-		//Tests for getStartYear and setStartYear
-		appointment.setStartYear(2018);
-		test = appointment.getStartYear();
-		assertEquals(2018, test);
-		 
-		//Tests for getTitle and setTitle
-		appointment.setTitle("Title");
-		String sTest = appointment.getTitle();
-		assertEquals("Title", sTest);
-		 
-		//Tests for getDescription and setDescription
-		appointment.setDescription("Description");
-		sTest = appointment.getDescription();
-		assertEquals("Description", sTest);
-		 
-		//Test for getValid
-		boolean bTrue = appointment.getValid();
-		assertEquals(true, bTrue);
-	}
-	 
-	/***************BRANCH COVERAGE FOR IS VALID()***************/
-	@Test
-	public void test02()  throws Throwable  {
-		Appt appointment = new Appt(4, 2, 2, 2, 2000, "T", "D");
-		 
-		//setHour test cases and isValid()
-		appointment.setStartHour(0);
-		boolean bTest = appointment.getValid();
-		assertEquals(false, bTest); //test lower edge case
+    @Test
+    public void test01() throws Throwable {
+        Appt appointment = new Appt(4, 2, 2, 2, 2000, "T", "D");
 
-		appointment.setStartHour(2);
-		bTest = appointment.getValid();
-		assertEquals(true, bTest); //test inner bound
-		
-		appointment.setStartHour(23);
-		bTest = appointment.getValid();
-		assertEquals(false, bTest); //test upper edge case
+        //Tests for getStartMonth and setStartMonth
+        appointment.setStartMonth(7);
+        int test = appointment.getStartMonth();
+        assertEquals(7, test);
 
-		appointment.setStartHour(-1);
-		bTest = appointment.getValid();
-		assertEquals(false, bTest); //test lower bound
+        //Tests for getStartHour and setStartHour
+        appointment.setStartHour(12);
+        test = appointment.getStartHour();
+        assertEquals(12, test);
 
-		appointment.setStartHour(25);
-		bTest = appointment.getValid();
-		assertEquals(false, bTest); //test upper bound
-	}
-	
-	@Test
-	public void test03() throws Throwable {
-		Appt appointment = new Appt(4, 2, 2, 2, 2000, "T", "D");
+        //Tests for getStartMinute and setStartMinute
+        appointment.setStartMinute(32);
+        test = appointment.getStartMinute();
+        assertEquals(32, test);
 
-		//setMinute test cases and isValid()
-		appointment.setStartMinute(0);
-		boolean bTest = appointment.getValid();
-		assertEquals(true, bTest); //test lower edge case
-		
-		appointment.setStartMinute(59);
-		bTest = appointment.getValid();
-		assertEquals(true, bTest); //test upper edge case
+        //Tests for getStartDay and setStartDay
+        appointment.setStartDay(2);
+        test = appointment.getStartDay();
+        assertEquals(2, test);
 
-		appointment.setStartMinute(2);
-		bTest = appointment.getValid();
-		assertEquals(true, bTest); //test inner bound
+        //Tests for getStartYear and setStartYear
+        appointment.setStartYear(2018);
+        test = appointment.getStartYear();
+        assertEquals(2018, test);
 
-		appointment.setStartMinute(-1);
-		bTest = appointment.getValid();
-		assertEquals(false, bTest); //test lower bound
+        //Tests for getTitle and setTitle
+        appointment.setTitle("Title");
+        String sTest = appointment.getTitle();
+        assertEquals("Title", sTest);
 
-		appointment.setStartMinute(64);
-		bTest = appointment.getValid();
-		assertEquals(false, bTest); //test upper bound
-	}
+        //Tests for getDescription and setDescription
+        appointment.setDescription("Description");
+        sTest = appointment.getDescription();
+        assertEquals("Description", sTest);
 
-	@Test
-	public void test04() throws Throwable {
-		Appt appointment = new Appt(4, 2, 2, 1, 2000, "T", "D");
+        //Test for getValid
+        boolean bTrue = appointment.getValid();
+        assertEquals(true, bTrue);
+    }
 
-		//setDay test cases and isValid()
-		appointment.setStartDay(1);
-		boolean bTest = appointment.getValid();
-		assertEquals(true, bTest); //test lower edge case
-		
-		appointment.setStartDay(31);
-		bTest = appointment.getValid();
-		assertEquals(true, bTest); //test upper edge case
+    /***************BRANCH COVERAGE FOR IS VALID()***************/
+    @Test
+    public void test02() throws Throwable {
+        Appt appointment = new Appt(4, 2, 2, 2, 2000, "T", "D");
 
-		appointment.setStartDay(4);
-		bTest = appointment.getValid();
-		assertEquals(true, bTest); //test inner bound
+        //setHour test cases and isValid()
+        appointment.setStartHour(-1);
+        boolean bTest = appointment.getValid();
+        assertEquals(false, bTest); //test lower edge case
 
-		appointment.setStartDay(-1);
-		bTest = appointment.getValid();
-		assertEquals(false, bTest); //test lower bound
+        appointment.setStartHour(2);
+        bTest = appointment.getValid();
+        assertEquals(true, bTest); //test inner bound
 
-		appointment.setStartDay(40);
-		bTest = appointment.getValid();
-		assertEquals(false, bTest); //test upper bound
-	}
+        appointment.setStartHour(23);
+        bTest = appointment.getValid();
+        assertEquals(false, bTest); //test upper edge case
 
-	@Test
-	public void test05() throws Throwable {
-		Appt appointment = new Appt(4, 2, 2, 2, 2000, "T", "D");
+        appointment.setStartHour(-1);
+        bTest = appointment.getValid();
+        assertEquals(false, bTest); //test lower bound
 
-		//setDay test cases and isValid()
-		appointment.setStartMonth(1);
-		boolean bTest = appointment.getValid();
-		assertEquals(true, bTest); //test lower edge case
-		
-		appointment.setStartMonth(12);
-		bTest = appointment.getValid();
-		assertEquals(true, bTest); //test upper edge case
+        appointment.setStartHour(25);
+        bTest = appointment.getValid();
+        assertEquals(false, bTest); //test upper bound
+    }
 
-		appointment.setStartMonth(4);
-		bTest = appointment.getValid();
-		assertEquals(true, bTest); //test inner bound
-		
-		int err = 0;
-		try {
-			appointment.setStartMonth(0);
-		} catch (java.lang.ArrayIndexOutOfBoundsException e) {
-			err = 1;
-		}
-		if (err == 1) {
-			bTest = false;
-		}
-		else {
-			bTest = appointment.getValid();
-		}
-		assertEquals(false, bTest); //test lower bound
-		
-		err = 0;
-		try {
-			appointment.setStartMonth(40);
-		} catch (java.lang.ArrayIndexOutOfBoundsException e) {
-			err = 1;
-		}
-		if (err == 1) {
-			bTest = false;
-		}
-		else {
-			bTest = appointment.getValid();
-		}
-		assertEquals(false, bTest); //test upper bound
-	}
+    @Test
+    public void test03() throws Throwable {
+        Appt appointment = new Appt(4, 2, 2, 2, 2000, "T", "D");
 
-	@Test
-	public void test06()  throws Throwable  {
-		//setRecurBy
-		Appt appointment = new Appt(4, 2, 2, 2, 2000, "T", "D");
-		
-		appointment.setRecurrence(null, 5, 5, 5);
-		
-		int iTest = appointment.getRecurNumber();
-		assertEquals(iTest, 5);
-		 
-		iTest = appointment.getRecurBy();
-		assertEquals(iTest, 5);
-		 
-		int[] iArrTest = appointment.getRecurDays();
-		
-		//isRecurring
-		boolean bTest = appointment.isRecurring();
-		assertEquals(bTest, true);
-		
-		iTest = appointment.getRecurIncrement();
-		assertEquals(iTest, 5);
-	}
-	
-	@Test
-	public void test07() throws Throwable {
-		//toString, representationApp, and compareTo test
-		Appt appt = new Appt(12, 2, 2, 2, 2000, "T", "D");
-		Appt appt2 = new Appt(3, 1, 1, 1, 1999, "T", "D");
-		Appt appt3 = new Appt(0, 0, 1, 2, 2000, "T", "D");
+        //setMinute test cases and isValid()
+        appointment.setStartMinute(0);
+        boolean bTest = appointment.getValid();
+        assertEquals(true, bTest); //test lower edge case
 
-		String sTest = appt.toString();
-		assertEquals("\t2/2/2000 at 12:2pm ,T, D\n", sTest);
-		sTest = appt2.toString();
-		assertEquals("\t1/1/1999 at 3:1am ,T, D\n", sTest);
-		
-		int iTest = appt.compareTo(appt2);
-		assertEquals(13, iTest);
-		
-		sTest = appt3.toString();
-		assertEquals(null, sTest);
-	}
+        appointment.setStartMinute(59);
+        bTest = appointment.getValid();
+        assertEquals(true, bTest); //test upper edge case
+
+        appointment.setStartMinute(2);
+        bTest = appointment.getValid();
+        assertEquals(true, bTest); //test inner bound
+
+        appointment.setStartMinute(-1);
+        bTest = appointment.getValid();
+        assertEquals(false, bTest); //test lower bound
+
+        appointment.setStartMinute(64);
+        bTest = appointment.getValid();
+        assertEquals(false, bTest); //test upper bound
+    }
+
+    @Test
+    public void test04() throws Throwable {
+        Appt appointment = new Appt(4, 2, 2, 1, 2000, "T", "D");
+
+        //setDay test cases and isValid()
+        appointment.setStartDay(1);
+        boolean bTest = appointment.getValid();
+        assertEquals(true, bTest); //test lower edge case
+
+        appointment.setStartDay(31);
+        bTest = appointment.getValid();
+        assertEquals(true, bTest); //test upper edge case
+
+        appointment.setStartDay(4);
+        bTest = appointment.getValid();
+        assertEquals(true, bTest); //test inner bound
+
+        appointment.setStartDay(-1);
+        bTest = appointment.getValid();
+        assertEquals(false, bTest); //test lower bound
+
+        appointment.setStartDay(40);
+        bTest = appointment.getValid();
+        assertEquals(false, bTest); //test upper bound
+    }
+
+    @Test
+    public void test05() throws Throwable {
+        Appt appointment = new Appt(4, 2, 2, 2, 2000, "T", "D");
+
+        //setDay test cases and isValid()
+        appointment.setStartMonth(1);
+        boolean bTest = appointment.getValid();
+        assertEquals(true, bTest); //test lower edge case
+
+        appointment.setStartMonth(12);
+        bTest = appointment.getValid();
+        assertEquals(true, bTest); //test upper edge case
+
+        appointment.setStartMonth(4);
+        bTest = appointment.getValid();
+        assertEquals(true, bTest); //test inner bound
+
+        int err = 0;
+        try {
+            appointment.setStartMonth(0);
+        } catch (java.lang.ArrayIndexOutOfBoundsException e) {
+            err = 1;
+        }
+        if (err == 1) {
+            bTest = false;
+        } else {
+            bTest = appointment.getValid();
+        }
+        assertEquals(false, bTest); //test lower bound
+
+        err = 0;
+        try {
+            appointment.setStartMonth(40);
+        } catch (java.lang.ArrayIndexOutOfBoundsException e) {
+            err = 1;
+        }
+        if (err == 1) {
+            bTest = false;
+        } else {
+            bTest = appointment.getValid();
+        }
+        assertEquals(false, bTest); //test upper bound
+    }
+
+    @Test
+    public void test06() throws Throwable {
+        //setRecurBy
+        Appt appointment = new Appt(4, 2, 2, 2, 2000, "T", "D");
+
+        appointment.setRecurrence(null, 5, 5, 5);
+
+        int iTest = appointment.getRecurNumber();
+        assertEquals(iTest, 5);
+
+        iTest = appointment.getRecurBy();
+        assertEquals(iTest, 5);
+
+        int[] iArrTest = appointment.getRecurDays();
+
+        //isRecurring
+        boolean bTest = appointment.isRecurring();
+        assertEquals(bTest, true);
+
+        iTest = appointment.getRecurIncrement();
+        assertEquals(iTest, 5);
+    }
+
+    @Test
+    public void test07() throws Throwable {
+        //toString, representationApp, and compareTo test
+        Appt appt = new Appt(12, 2, 2, 2, 2000, "T", "D");
+        Appt appt2 = new Appt(3, 1, 1, 1, 1999, "T", "D");
+        Appt appt3 = new Appt(0, -1, 1, 2, 2000, "T", "D");
+
+        String sTest = appt.toString();
+        assertEquals("\t2/2/2000 at 12:2pm ,T, D\n", sTest);
+        sTest = appt2.toString();
+        assertEquals("\t1/1/1999 at 3:1am ,T, D\n", sTest);
+
+        int iTest = appt.compareTo(appt2);
+        assertEquals(13, iTest);
+
+        sTest = appt3.toString();
+        assertEquals(null, sTest);
+    }
 }
--- projects/gilesni/Assignment-5/Brenisec/Calendar/src/test/java/calendar/TimeTableTest.java
+++ projects/gilesni/Assignment-5/Brenisec/Calendar/src/test/java/calendar/TimeTableTest.java
@@ -1,59 +1,62 @@
 package calendar;
 /**
- *  This class provides a basic set of test cases for the
- *  TimeTableTest.
+ * This class provides a basic set of test cases for the
+ * TimeTableTest.
  */
+
 import org.junit.Test;
+
 import java.util.*;
 
 import static org.junit.Assert.*;
+
 public class TimeTableTest {
-	@Test
-	public void test01()  throws Throwable  {
-		TimeTable timeTable = new TimeTable();
-		 
-		GregorianCalendar thirteen = new GregorianCalendar(2016, 05, 13);
-		GregorianCalendar fourteen = new GregorianCalendar(2016, 05, 14);
-		GregorianCalendar fifteen = new GregorianCalendar(2016, 05, 15);
-		 
-		Appt day1_1 = new Appt(12, 2, 13, 5, 2016, "T", "D");
-		Appt day1_2 = new Appt(15, 2, 13, 5, 2016, "T", "D");
+    @Test
+    public void test01() throws Throwable {
+        TimeTable timeTable = new TimeTable();
 
-		Appt day2_1 = new Appt(12, 2, 14, 5, 2016, "T", "D");
-		Appt day2_2 = new Appt(15, 2, 14, 5, 2016, "T", "D");
-		 
-		LinkedList<CalDay> calDays = new LinkedList<CalDay>();
-		
-		LinkedList<Appt> appts = new LinkedList<Appt>();	
-		appts.add(day1_1);
-		appts.add(day1_2);
-		appts.add(day2_1);
-		appts.add(day2_2);
+        GregorianCalendar thirteen = new GregorianCalendar(2016, 05, 13);
+        GregorianCalendar fourteen = new GregorianCalendar(2016, 05, 14);
+        GregorianCalendar fifteen = new GregorianCalendar(2016, 05, 15);
 
-		calDays = timeTable.getApptRange(appts, thirteen, fifteen);
+        Appt day1_1 = new Appt(12, 2, 13, 5, 2016, "T", "D");
+        Appt day1_2 = new Appt(15, 2, 13, 5, 2016, "T", "D");
 
-		LinkedList<CalDay> calDayCheck = new LinkedList<CalDay>();
-		CalDay calday1 = new CalDay(thirteen);
-		CalDay calday2 = new CalDay(fourteen);
+        Appt day2_1 = new Appt(12, 2, 14, 5, 2016, "T", "D");
+        Appt day2_2 = new Appt(15, 2, 14, 5, 2016, "T", "D");
 
-		calday1.addAppt(day1_1);
-		//calday1.addAppt(day1_2);
+        LinkedList<CalDay> calDays = new LinkedList<CalDay>();
 
-		calday2.addAppt(day2_1);
-		//calday2.addAppt(day2_2);
+        LinkedList<Appt> appts = new LinkedList<Appt>();
+        appts.add(day1_1);
+        appts.add(day1_2);
+        appts.add(day2_1);
+        appts.add(day2_2);
 
-		calDayCheck.add(calday1);
-		calDayCheck.add(calday2);
-		
-		String sCheck = calday1.toString() + calday2.toString();
-		
-		Iterator<CalDay> itr = calDays.iterator();
-		CalDay calDayTest1 = itr.next();
-		CalDay calDayTest2 = itr.next();
-		String sTest = calDayTest1.toString() + calDayTest2.toString();
+        calDays = timeTable.getApptRange(appts, thirteen, fifteen);
 
-		assertEquals(sCheck, sTest);
-	 }
+        LinkedList<CalDay> calDayCheck = new LinkedList<CalDay>();
+        CalDay calday1 = new CalDay(thirteen);
+        CalDay calday2 = new CalDay(fourteen);
+
+        calday1.addAppt(day1_1);
+        //calday1.addAppt(day1_2);
+
+        calday2.addAppt(day2_1);
+        //calday2.addAppt(day2_2);
+
+        calDayCheck.add(calday1);
+        calDayCheck.add(calday2);
+
+        String sCheck = calday1.toString() + calday2.toString();
+
+        Iterator<CalDay> itr = calDays.iterator();
+        CalDay calDayTest1 = itr.next();
+        CalDay calDayTest2 = itr.next();
+        String sTest = calDayTest1.toString() + calDayTest2.toString();
+
+        assertEquals(sCheck, sTest);
+    }
 	 
 	 /*@Test Works, but throws the dateOutOfRange exception which is seen as an error
 	 public void test02() throws Throwable {
@@ -76,37 +79,37 @@ public class TimeTableTest {
 		calDayCheck = timeTable.getApptRange(llCheck, tomorrow, today);
 		assertEquals(calDays, calDayCheck);
 	 }*/
-	 
-	 @Test
-	 public void test03() throws Throwable {
-		TimeTable timeTable = new TimeTable();
-			 
-		GregorianCalendar today = new GregorianCalendar(2016, 05, 13);
-		GregorianCalendar tomorrow = new GregorianCalendar(2016, 05, 15);
-			 
-		Appt appt = new Appt(12, 2, 13, 5, 2016, "T", "D");
-		Appt appt2 = new Appt(3, 2, 13, 5, 2016, "T", "D");
-		Appt appt3 = new Appt(4, 2, 14, 5, 2016, "T", "D");
-		Appt appt4 = new Appt(5, 2, 14, 5, 2016, "T", "D");
-		Appt appt5 = new Appt(6, 2, 15, 5, 2016, "T", "D");
-		
-		LinkedList<Appt> appts = new LinkedList<Appt>();
-		appts.add(appt);
-		appts.add(appt2);
-		appts.add(appt3);
-		appts.add(appt4);
-		appts.add(appt5);
-		 
-		LinkedList<CalDay> calDays = new LinkedList<CalDay>();
-		LinkedList<CalDay> calDayCheck = new LinkedList<CalDay>();
-		
-		LinkedList<Appt> llcheck = new LinkedList<Appt>();
-		llcheck.add(appt);
-		llcheck.add(appt2);
-		llcheck.add(appt3);
-		llcheck.add(appt5);
 
-		assertEquals(llcheck, timeTable.deleteAppt(appts, appt4));
-		assertEquals(null, timeTable.deleteAppt(appts, null));
-	 }
+    @Test
+    public void test03() throws Throwable {
+        TimeTable timeTable = new TimeTable();
+
+        GregorianCalendar today = new GregorianCalendar(2016, 05, 13);
+        GregorianCalendar tomorrow = new GregorianCalendar(2016, 05, 15);
+
+        Appt appt = new Appt(12, 2, 13, 5, 2016, "T", "D");
+        Appt appt2 = new Appt(3, 2, 13, 5, 2016, "T", "D");
+        Appt appt3 = new Appt(4, 2, 14, 5, 2016, "T", "D");
+        Appt appt4 = new Appt(5, 2, 14, 5, 2016, "T", "D");
+        Appt appt5 = new Appt(6, 2, 15, 5, 2016, "T", "D");
+
+        LinkedList<Appt> appts = new LinkedList<Appt>();
+        appts.add(appt);
+        appts.add(appt2);
+        appts.add(appt3);
+        appts.add(appt4);
+        appts.add(appt5);
+
+        LinkedList<CalDay> calDays = new LinkedList<CalDay>();
+        LinkedList<CalDay> calDayCheck = new LinkedList<CalDay>();
+
+        LinkedList<Appt> llcheck = new LinkedList<Appt>();
+        llcheck.add(appt);
+        llcheck.add(appt2);
+        llcheck.add(appt3);
+        llcheck.add(appt5);
+
+        assertEquals(llcheck, timeTable.deleteAppt(appts, appt4));
+        assertEquals(null, timeTable.deleteAppt(appts, null));
+    }
 }
